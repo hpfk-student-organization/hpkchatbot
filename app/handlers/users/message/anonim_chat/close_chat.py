020fb0ca104d46583169e86eadbce87b91dd7891
Alex <alosha123456789049@gmail.com>
2023-01-29 16:31:33 +0200

    Hot fix anonim chat

--- app/handlers/users/message/anonim_chat/close_chat.py
+++ app/handlers/users/message/anonim_chat/close_chat.py
@@ -10,11 +10,11 @@ from states import AnonymousChatStates
 
 @router.message(Text(text=AnonymousChatKb.search_again_btn), StateFilter(AnonymousChatStates.close_chat))
 async def search_again(message: types.Message, state: FSMContext, bot: aiogram.Bot):
-    from handlers import search_user_to_talk
+    from handlers.users.message.anonim_chat.start_search import search_user_to_talk
     await search_user_to_talk(message=message, state=state, bot=bot)
 
 
 @router.message(Text(text=AnonymousChatKb.main_menu_btn), StateFilter(AnonymousChatStates.close_chat))
 async def exit_to_main_menu(message: types.Message, state: FSMContext):
-    from handlers import anonymous_chat_button
+    from handlers.users.message.main import anonymous_chat_button
     await anonymous_chat_button(message=message, state=state)
